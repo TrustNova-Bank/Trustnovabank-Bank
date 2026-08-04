6f40eff1ceed89cf4e399660f838b1cc1da890dd
trustnovabank.com <destinyluck43@gmail.com>
2026-08-04 14:46:25 -0400

    Update supabase.js

--- shared/supabase.js
+++ shared/supabase.js
@@ -1,18 +1,31 @@
 const SUPABASE_URL =
+
     "https://ftcmobswvxyhcheabtet.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY =
-    "YOUR_PUBLISHABLE_KEY";
+
+    "sb_publishable_8n4fnNTInYP5fR1Z4e706Q_Z-n913B8";
 
 const supabase =
+
     window.supabase.createClient(
+
         SUPABASE_URL,
+
         SUPABASE_PUBLISHABLE_KEY,
+
         {
+
             auth: {
+
                 autoRefreshToken: true,
+
                 persistSession: true,
+
                 detectSessionInUrl: true
+
             }
+
         }
-    );
+
+    )
